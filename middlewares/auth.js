6557f46d6cb8556194094239d1c2e714196b2c4e
Scumsy <dmitry.koschavka@gmail.com>
2023-03-26 18:49:23 +0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,7 +8,7 @@ const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
   try {
-    if (bearer !== "Bearer") {
+    if (bearer !== "Bearer" && !token) {
       next(HttpError(401));
       //   res.status(401).json({ message: "Not authorized" });
     }
